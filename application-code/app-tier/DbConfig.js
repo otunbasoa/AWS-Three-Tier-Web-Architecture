dded9d2f7c0cd82be199ac9960d9f551f9eb4695
otunbasoa <[anjorinolu.samson@gmail.com]>
2023-09-08 11:19:17 +0100

    Initial commit

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.cvehehq9dhur.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Samsungractor1',
+    DB_DATABASE : 'webappdb'
+});
